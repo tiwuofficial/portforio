89320c03b503da546d6b0bd5709cd59a140d11a9
田口航 <tiwu@wataru.local>
2016-06-22 01:22:42 +0900

    修正

--- js/common.js
+++ js/common.js
@@ -1,22 +1,17 @@
-$(function() {
+(function($) {
 
-    // lazyLoadがclassに指定されているimg要素の画像読込を開始する
-    var $imgList = $('img.lazyLoad');
-    $imgList.each(function() {
-        var $target = $(this);
+    function startLazyLoad($imgList) {
+        $imgList.each(function() {
+            var $target = $(this);
+            var imgUrl = $target.attr('alt');
 
-        // 画像の指定がない場合はNoImage画像を設定
-        var imgUrl = $target.attr('alt');
-
-        // 画像の読み込み時のイベントに処理を設定
-        $target.on('load', function() {
-            $target.removeClass('lazyLoad');
-            $target.parent().find('.fa-spinner').css('display', 'none');
-        });
-
-        // 画像の読み込みを開始
-        $target.attr('src', imgUrl);
-    })
+            $target.on('load', function() {
+                $target.removeClass('lazyLoad');
+                $target.parent().find('.fa-spinner').css('display', 'none');
+            });
+            $target.attr('src', imgUrl);
+        })
+    }
 
     $('#content').infinitescroll({
         navSelector: ".navigation",
@@ -27,6 +22,14 @@ $(function() {
         loading: {
             finishedMsg: "",
             msgText: "",
+            img: "",
         }
+    }, function() {
+        startLazyLoad($('img.lazyLoad'));
     });
-});
+
+    $(function() {
+        startLazyLoad($('img.lazyLoad'));
+    });
+
+})($);
